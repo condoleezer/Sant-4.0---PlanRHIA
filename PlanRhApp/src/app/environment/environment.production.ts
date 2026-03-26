97a4895e78d80551106f03bb6cacadd3026f94e6
condoleezer <madaveisabelle@gmail.com>
2026-03-26 12:01:22 +0100

    correction front

--- PlanRhApp/src/app/environment/environment.production.ts
+++ PlanRhApp/src/app/environment/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   title: 'PlanRH APP',
-  apiUrl: 'https://londo-todo-list-test.onrender.com/api/v1',
+  apiUrl: 'https://planrhia.onrender.com',
 };
